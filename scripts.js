7dc1d69db91d94be16c07641a63b7b64b391c68a
Krishnanand G <krishnanandgeetheswaran@gmail.com>
2023-05-31 08:16:16 +0530

    added wrong word functionality

--- scripts.js
+++ scripts.js
@@ -1,4 +1,4 @@
-let body = document.querySelector('body');
+let errorMessage = null;
 
 document.querySelector('.search-form').addEventListener('submit', async (event) => {
     event.preventDefault();
@@ -10,10 +10,28 @@ document.querySelector('.search-form').addEventListener('submit', async (event) 
       const response = await fetch(`https://api.dictionaryapi.dev/api/v2/entries/en/${inputValue}`);
       const data = await response.json();
       console.log(data);
+
+      if(data.title === 'No Definitions Found') {
+        displayErrorMessage();
+      } else {
+        removeErrorMessage();
+      }
     } catch (error) {
       console.log(error);
     }
 
     document.querySelector('.search').value = "";
   });
+
+  function displayErrorMessage() {
+    errorMessage = document.createElement('p');
+    errorMessage.innerHTML = '<strong>No Such Word Is Found</strong>'
+    document.querySelector('.search-form').insertAdjacentElement('afterend', errorMessage);
+  }
   
+  function removeErrorMessage() {
+    if (errorMessage) {
+      errorMessage.remove();
+      errorMessage = null;
+    }
+  }
